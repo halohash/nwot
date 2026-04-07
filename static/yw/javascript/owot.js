7b8af40b3b81cd10dda0772b5be8cc2507544a98
giggityfiles <giggityfiles@gmail.com>
2026-04-07 16:23:36 -0400

    additions

--- static/yw/javascript/owot.js
+++ static/yw/javascript/owot.js
@@ -5168,7 +5168,7 @@ function buildMenu() {
 	homeLink.href = "/home";
 	homeLink.target = "_blank";
 	homeLink.innerHTML = "More...&nbsp";
-	homeLinkIcon.src = "/static/link.svg";
+	homeLinkIcon.src = "https://halohash.github.io/worldofhash/static/link.svg";
 	homeLinkIcon.style.width = "12px";
 	homeLinkIcon.style.height = "12px";
 	homeLink.appendChild(homeLinkIcon);
@@ -8235,7 +8235,7 @@ function begin() {
 	protectPrecisionOption(protectPrecision);
 
 	if(state.userModel.is_superuser) {
-		w.loadScript("/static/yw/javascript/world_tools.js");
+		w.loadScript("https://halohash.github.io/worldofhash/static/yw/javascript/world_tools.js");
 	}
 	
 	if(state.worldModel.default_script_path && window.URL) {
